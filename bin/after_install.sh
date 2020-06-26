0aa6881d751d8b815f32d0171a1896ecca147b4c
nourlatreche <nour@elementarydigital.co.uk>
2020-06-26 11:07:17 +0100

    move to bin 8

--- bin/after_install.sh
+++ bin/after_install.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-#fixowns
-sh /var/www/html/WordPress/bin/test.sh
+
+APP_PATH='/var/www/html/magento-app/bin'
+
+sh $APP_PATH/env
+sh $APP_PATH/composer install
+sh $APP_PATH/magento module:enable --all
+sh $APP_PATH/magento setup:upgrade
+sh $APP_PATH/magento setup:static-content:deploy -f
+sh $APP_PATH/magento setup:di:compile
+sh $APP_PATH/fixowns
+
--- bin/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker exec  --user=root 5155648e6e7c mkdir xoxo
-
