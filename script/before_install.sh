40566d750507d9f70ebe6c49ce2b1888b8ad6b5c
nourlatreche <nour@elementarydigital.co.uk>
2020-06-26 11:46:14 +0100

    move to bin 16

--- script/before_install.sh
+++ script/before_install.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-cp -r /var/www/html/magento-app/aws/* /var/www/html/magento-app/src
+cp -Rf /var/www/html/magento-app/aws/* /var/www/html/magento-app/src
+chmod +x  /var/www/html/magento-app/src/script/*
