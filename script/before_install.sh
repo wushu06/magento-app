2fd98cc02acdef7d6a9d06ece59c83dc3d6fe247
nourlatreche <nour@elementarydigital.co.uk>
2020-06-26 14:46:07 +0100

    remove content

--- script/before_install.sh
+++ script/before_install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm -r /var/www/html/magento-app/src/*
 \cp -Rf /var/www/html/magento-app/aws/* /var/www/html/magento-app/src
 \cp -Rf /var/www/html/magento-app/shared/* /var/www/html/magento-app/src
 chmod +x  /var/www/html/magento-app/src/script/*
