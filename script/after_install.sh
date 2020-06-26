781943b77666a2a46bd24ce5f74e0bf247bdaa97
nourlatreche <nour@elementarydigital.co.uk>
2020-06-26 11:18:18 +0100

    move to bin 10

--- script/after_install.sh
+++ script/after_install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-APP_PATH='/var/www/html/magento-app/src/bin'
+APP_PATH='/var/www/html/magento-app/src/script'
 
-sh $APP_PATH/env
 sh $APP_PATH/composer install
+sh $APP_PATH/env
 sh $APP_PATH/magento module:enable --all
 sh $APP_PATH/magento setup:upgrade
 sh $APP_PATH/magento setup:static-content:deploy -f
