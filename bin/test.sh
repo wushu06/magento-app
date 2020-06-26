e15385cc5a7b3103837a4f3accd4b23d8b103e9e
nourlatreche <nour@elementarydigital.co.uk>
2020-06-26 10:40:05 +0100

    changed after install 3

---
+++ bin/test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker exec -it --user=root 5155648e6e7c mkdir xoxo
+
--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-mkdir -p /var/www/html/worked
-
-cat <<EOF | sudo tee /var/www/html/worked/deploy.php
-working fine.
-EOF
-
+bin/fixowns
+bin/test
