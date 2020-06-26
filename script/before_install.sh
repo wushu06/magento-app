c2275a6e0a4b1123a6d4a2111d57cd78e1559bc7
nourlatreche <nour@elementarydigital.co.uk>
2020-06-26 14:38:19 +0100

    change scripts

--- script/before_install.sh
+++ script/before_install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 \cp -Rf /var/www/html/magento-app/aws/* /var/www/html/magento-app/src
+\cp -Rf /var/www/html/magento-app/shared/* /var/www/html/magento-app/src
 chmod +x  /var/www/html/magento-app/src/script/*
+
+
