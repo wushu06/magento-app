1a1d11f6b4349eea3f1fae42fb7d919e0fa0c6c0
nourlatreche <nour@elementarydigital.co.uk>
2020-06-26 15:02:08 +0100

    add release 2

--- script/before_install.sh
+++ script/before_install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-mkdir -p /var/www/html/magento-app/src/app/release
-mkdir -p /var/www/html/magento-app/src/app/aws
+ROOT=/var/www/html/magento-app
 
-\cp -Rf /var/www/html/magento-app/aws/* /var/www/html/magento-app/release
-\cp -Rf /var/www/html/magento-app/shared/* /var/www/html/magento-app/release
-chmod +x  /var/www/html/magento-app/release/script/*
+mkdir -p $ROOT/release
+mkdir -p $ROOT/aws
+
+\cp -Rf $ROOT/aws/* $ROOT/release
+\cp -Rf $ROOT/shared/* $ROOT/release
+chmod +x  $ROOT/release/script/*
 
 
--- script/start_server.sh
+++ script/start_server.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # remove and past content to avoid container mapping issue
-rm -r /var/www/html/magento-app/src/*
-\cp -Rf /var/www/html/magento-app/release/* /var/www/html/magento-app/src
-chmod +x  /var/www/html/magento-app/src/script/*
+ROOT=/var/www/html/magento-app
+rm -r $ROOT/src/*
+\cp -Rf $ROOT/release/* $ROOT/src
+chmod +x  $ROOT/src/script/*
 
-APP_PATH='/var/www/html/magento-app/src/script'
+APP_PATH=$ROOT/src/script
 sh $APP_PATH/fixowns
