d980ccd7264d2a9bc77b433f30ae9be590fcc361
nourlatreche <nour@elementarydigital.co.uk>
2020-06-26 16:10:54 +0100

    add release 7

--- script/after_install.sh
+++ script/after_install.sh
@@ -3,7 +3,6 @@
 APP_PATH='/var/www/html/magento-app/release/script'
 
 sh $APP_PATH/composer install
-sh $APP_PATH/env
 sh $APP_PATH/magento module:enable --all
 sh $APP_PATH/magento setup:upgrade
 sh $APP_PATH/magento setup:static-content:deploy -f
