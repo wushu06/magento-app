393783fd53a83b398008e0b650b7d4cc12e9b55b
nourlatreche <nour@elementarydigital.co.uk>
2020-06-26 22:15:58 +0100

    symlink 2

--- script/start_server.sh
+++ script/start_server.sh
@@ -7,6 +7,7 @@ chmod +x  $ROOT/src/script/*
 
 APP_PATH=$ROOT/src/script
 
+rm /var/www/html/src/app/etc/env.php
 sh $APP_PATH/cli ln -sf /var/www/html/shared/env.php /var/www/html/src/app/etc/env.php
 
 sh $APP_PATH/fixowns
