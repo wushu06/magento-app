66e7b1e3f59926b19c84a2277c22b4a4d4cfcf5a
nourlatreche <nour@elementarydigital.co.uk>
2020-07-06 13:29:08 +0100

    new build v5

--- script/before_install.sh
+++ script/before_install.sh
@@ -4,9 +4,11 @@ docker rm $(docker ps -a -f status=exited -q)
 ROOT=/var/www/html
 
 if [ ! -d $ROOT/release ]; then
+    echo 'creating release folder..'
     mkdir -p $ROOT/release;
 fi;
 if [ ! -d $ROOT/git ]; then
+    echo 'creating git folder..'
     mkdir -p $ROOT/git;
 fi;
 sudo rm -rf $ROOT/release/*
