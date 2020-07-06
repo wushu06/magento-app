c28d6fe47de9b01ee798c9cd036525962f660d17
nourlatreche <nour@elementarydigital.co.uk>
2020-07-06 13:25:49 +0100

    new build v3

--- script/before_install.sh
+++ script/before_install.sh
@@ -6,11 +6,11 @@ ROOT=/var/www/html
 if [ ! -d $ROOT/release ]; then
     mkdir -p $ROOT/release;
 fi;
-if [ ! -d $ROOT/aws ]; then
-    mkdir -p $ROOT/aws;
+if [ ! -d $ROOT/git ]; then
+    mkdir -p $ROOT/git;
 fi;
 sudo rm -rf $ROOT/release/*
-\cp -Rf $ROOT/aws/* $ROOT/release
+\cp -Rf $ROOT/git/* $ROOT/release
 \cp -Rf $ROOT/shared/auth.json $ROOT/release/auth.json
 chmod +x  $ROOT/release/script/*
 
